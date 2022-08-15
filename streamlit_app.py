a65820f656194a0ec69e12ba4daba7cfcce27a4f
TradeObjetivo <76456782+Lucas1391@users.noreply.github.com>
2022-08-15 09:59:58 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,10 +67,11 @@ if taxa:
     }))
 
     
-     chart_data = pd.DataFrame(
-     filtro['Taxa Compra Manha'],filtro['Taxa Compra Manha'].rolling(200).mean()
-     columns=['Taxa', 'Mediana'])
+    chart_data = pd.DataFrame(
+    filtro['Taxa Compra Manha'],filtro['Taxa Compra Manha'].rolling(200).mean()
+    columns=['Taxa', 'Mediana'])
 
+    st.text("Grafico Taxa e Mediana Móvel")
     st.line_chart(chart_data)
     st.text(f'A janela de compra com a taxa de {taxa} é uma janela {janela}')
 
